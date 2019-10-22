a0dad3c7ef12a43c7bbf82fb89c510338f78fdfa
Michaël Defferrard <michael.defferrard@epfl.ch>
2019-10-22 22:58:06 +0200

    trim whitespace

--- assignments/utils.py
+++ assignments/utils.py
@@ -1,7 +1,8 @@
 from sklearn.datasets import fetch_openml
 import numpy as np
 
-def __subsample_mnist(images, labels, n):
+
+def _subsample_mnist(images, labels, n):
 
     indices_to_keep = np.array([], dtype=np.int)
     classes = np.unique(labels)
@@ -11,7 +12,7 @@ def __subsample_mnist(images, labels, n):
         if i in classes:
             belongs_to_class_i = np.where(labels==i)[0]
             indices_to_keep = np.append(indices_to_keep, belongs_to_class_i[:keep_by_class])
-    
+
     X = images[indices_to_keep]
     Y = labels[indices_to_keep]
     return X, Y
@@ -19,16 +20,16 @@ def __subsample_mnist(images, labels, n):
 
 def load_mnist(path='./data/mnist/'):
     # Load dataset
-    
+
     mnist = fetch_openml('mnist_784')
     images =  mnist['data'][:10000]
     labels = np.array(list(map(int, mnist['target'][:10000])))
-    
+
     classes = np.unique(labels)
     n_clusters_mnist = len(classes)
     keep_per_class = 300
     n = keep_per_class * len(classes)
     images = np.array(images)
     labels = np.array(labels)
-    
-    return __subsample_mnist(images, labels, n)
+
+    return _subsample_mnist(images, labels, n)
